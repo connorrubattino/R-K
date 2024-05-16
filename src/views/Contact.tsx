85806ef87ce33ff2c72a3f6914cb0c5ac5771217
Connor Rubattino <connor.rubattino@gmail.com>
2024-05-16 11:26:08 -0700

    coloring

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -10,15 +10,15 @@ export default function Contact({}: Props) {
     <Card className='card mt-5 card bg-white-transparent'>
       <Card.Header style={{ color: '#792E2F' }} className='text-center font-weight-bold' as="h2">Contact Us</Card.Header>
       <Card.Body>
-        <Card.Title>Email</Card.Title>
+        <Card.Title style={{ color: '#792E2F' }}>Email</Card.Title>
         <Card.Text>
           With supporting text below as a natural lead-in to additional content.
         </Card.Text>
-        <Card.Title>Phone</Card.Title>
+        <Card.Title style={{ color: '#792E2F' }}>Phone</Card.Title>
         <Card.Text>
           With supporting text below as a natural lead-in to additional content.
         </Card.Text>
-        <Card.Title>Houzz</Card.Title>
+        <Card.Title style={{ color: '#792E2F' }}>Houzz</Card.Title>
         <Card.Text>
           With supporting text below as a natural lead-in to additional content.
         </Card.Text>
